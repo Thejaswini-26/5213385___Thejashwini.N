079f99b832e161ee3cd4252a879a51c6b9bd678f
teju gowda <tejn0921@gmail.com>
2025-08-11 22:16:40 +0530

    Week1 Python code, due to error in C i.e., even after getting same as sample o/p, compiler displaying error as wrong output

--- HackerRank/Week 1/XOR_Strings.py
+++ HackerRank/Week 1/XOR_Strings.py
@@ -1,3 +1,5 @@
+#in C even after getting exactly as expected output(sample output), we getting as wrong answer.. that why i used Python code for this quetion
+
 def strings_xor(s, t):
     res = ""
     for i in range(len(s)):
